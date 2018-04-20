dd97267e65557d8d25a34df9f56a8e6f0d4ccaf9
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-04-20 21:51:48 +0300

    simplify equations

--- src/player/physics/calculateDirection.js
+++ src/player/physics/calculateDirection.js
@@ -1,74 +1,18 @@
 const calculateDirection = (playerData, otherPlayerData) => {
   if (playerData.vX > 0 && playerData.vY >= 0) {
-    if (playerData.x > otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x > otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return -1;
-    }
+    return playerData.x > otherPlayerData.x ? 1 : -1;
   }
 
   if (playerData.vX <= 0 && playerData.vY > 0) {
-    if (playerData.x > otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x > otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return -1;
-    }
+    return playerData.x > otherPlayerData.x ? 1 : -1;
   }
 
   if (playerData.vX >= 0 && playerData.vY < 0) {
-    if (playerData.x > otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x > otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return 1;
-    }
+    return playerData.x > otherPlayerData.x ? -1 : 1;
   }
 
   if (playerData.vX < 0 && playerData.vY <= 0) {
-    if (playerData.x > otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y > otherPlayerData.y) {
-      return 1;
-    }
-
-    if (playerData.x > otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return -1;
-    }
-
-    if (playerData.x < otherPlayerData.x && playerData.y < otherPlayerData.y) {
-      return 1;
-    }
+    return playerData.x > otherPlayerData.x ? -1 : 1;
   }
 
   throw new Error('This code should be unreachable');
