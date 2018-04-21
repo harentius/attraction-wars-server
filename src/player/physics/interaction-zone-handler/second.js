825a0f71926e6f8287b83c8d48cbb9350935af94
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-04-21 23:47:18 +0300

    replace 3 and 2 zones

--- src/player/physics/interaction-zone-handler/second.js
+++ src/player/physics/interaction-zone-handler/second.js
@@ -1,6 +1,25 @@
+const { RotationData } = require('../../PlayerData');
+const calculateDirection = require('../calculateDirection');
+
 const secondZoneHandler = (playerData, otherPlayerData) => {
+  const r = Math.sqrt(
+    (otherPlayerData.x - playerData.x) ** 2
+    + (otherPlayerData.y - playerData.y) ** 2,
+  );
+
+  let oldDirection = null;
+
+  if (playerData.rotationData.has(otherPlayerData.id)) {
+    oldDirection = playerData.rotationData.get(otherPlayerData.id).direction;
+  }
+
+  playerData.rotationData.set(otherPlayerData.id, new RotationData(
+    otherPlayerData.x,
+    otherPlayerData.y,
+    r,
+    oldDirection || calculateDirection(playerData, otherPlayerData),
+  ));
   playerData.boundedToPlayersData.set(otherPlayerData.id, otherPlayerData);
-  playerData.rotationData.delete(otherPlayerData.id);
   playerData.attractionData.delete(otherPlayerData.id);
 };
 
