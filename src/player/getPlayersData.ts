44997b7b02bd9be20ddf3d34aa566bf2734ceaf6
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-05-01 23:59:19 +0300

    cs fixes

--- src/player/getPlayersData.ts
+++ src/player/getPlayersData.ts
@@ -2,7 +2,7 @@ import { PlayerData } from './PlayerData';
 import { generateRandomColor } from '../utils';
 
 const getPlayersData = () => ({
-  demo_player1: new PlayerData(1650, 1650, 200, 'demo_player1', generateRandomColor()),
+  demo_player1: new PlayerData(1650, 1650, 200, 'demo_player1', generateRandomColor())
 });
 
 export default getPlayersData;
