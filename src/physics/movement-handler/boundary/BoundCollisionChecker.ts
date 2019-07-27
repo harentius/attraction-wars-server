f5e1b26baa0de62149094670dc35d1ac116b9136
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-07-27 14:53:11 +0300

    fix borders collision

--- src/physics/movement-handler/boundary/BoundCollisionChecker.ts
+++ src/physics/movement-handler/boundary/BoundCollisionChecker.ts
@@ -2,22 +2,38 @@ import config from '../../../config';
 import PlayerData from '../../../storage/PlayerData';
 
 class BoundCollisionChecker {
-  public isBoundCollision(playerData: PlayerData) {
+  public isBoundCollision(playerData: PlayerData): boolean {
     return this.isXBoundCollision(playerData) || this.isYBoundCollision(playerData);
   }
 
-  public isXBoundCollision(playerData: PlayerData) {
-    return (playerData.x < (config.worldBounds[0] + playerData.r))
+  public isXBoundCollision(playerData: PlayerData): boolean {
+    return this.isXLeftBoundCollision(playerData)
         ||
-      (playerData.x > (config.worldBounds[2] - playerData.r))
+      this.isXRightBoundCollision(playerData)
     ;
   }
 
-  public isYBoundCollision(playerData: PlayerData) {
-    return (playerData.y < (config.worldBounds[1] + playerData.r))
+  public isYBoundCollision(playerData: PlayerData): boolean {
+    return this.isYLeftBoundCollision(playerData)
         ||
-      (playerData.y > (config.worldBounds[3] - playerData.r))
+      this.isYRightBoundCollision(playerData)
     ;
+  }
+
+  public isXLeftBoundCollision(playerData: PlayerData): boolean {
+    return playerData.x < (config.worldBounds[0] + playerData.r);
+  }
+
+  public isXRightBoundCollision(playerData: PlayerData): boolean {
+    return playerData.x > (config.worldBounds[2] - playerData.r);
+  }
+
+  public isYLeftBoundCollision(playerData: PlayerData): boolean {
+    return playerData.y < (config.worldBounds[1] + playerData.r);
+  }
+
+  public isYRightBoundCollision(playerData: PlayerData) {
+    return playerData.y > (config.worldBounds[3] - playerData.r);
   }
 }
 
--- src/physics/movement-handler/boundary/CheckBoundCollisionMovementHandler.ts
+++ src/physics/movement-handler/boundary/CheckBoundCollisionMovementHandler.ts
@@ -1,6 +1,9 @@
 import MovementHandlerInterface from '../MovementHandlerInterface';
 import BoundCollisionChecker from './BoundCollisionChecker';
 import Player from '../../../player/Player';
+import config from '../../../config';
+
+const BORDER_MARGIN = 1;
 
 class CheckBoundCollisionMovementHandler implements MovementHandlerInterface {
   private boundCollisionChecker: BoundCollisionChecker;
@@ -20,12 +23,14 @@ class CheckBoundCollisionMovementHandler implements MovementHandlerInterface {
       playerData.vX *= -1;
       playerData.bonusVx *= -1;
       playerData.reactiveVx *= -1;
+      playerData.aX *= -1;
     }
 
     if (this.boundCollisionChecker.isYBoundCollision(playerData)) {
       playerData.vY *= -1;
       playerData.bonusVy *= -1;
       playerData.reactiveVy *= -1;
+      playerData.aY *= -1;
     }
 
     for (const rotationData of playerData.rotationData.values()) {
@@ -34,6 +39,22 @@ class CheckBoundCollisionMovementHandler implements MovementHandlerInterface {
 
     for (const gravityAssistData of playerData.gravityAssistData.values()) {
       gravityAssistData.direction *= -1;
+    }
+
+    if (this.boundCollisionChecker.isXLeftBoundCollision(playerData)) {
+      playerData.x = playerData.r + BORDER_MARGIN;
+    }
+
+    if (this.boundCollisionChecker.isXRightBoundCollision(playerData)) {
+      playerData.x = config.worldBounds[2] - playerData.r - BORDER_MARGIN;
+    }
+
+    if (this.boundCollisionChecker.isYLeftBoundCollision(playerData)) {
+      playerData.y = playerData.r + BORDER_MARGIN;
+    }
+
+    if (this.boundCollisionChecker.isYRightBoundCollision(playerData)) {
+      playerData.y =  config.worldBounds[3] - playerData.r - BORDER_MARGIN;
     }
   }
 }
