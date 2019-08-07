9b852ae0ed548427f3c112c5aa257a7ca99e9a57
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-08-08 00:03:20 +0300

    Revert "tmp disable asteroids sync"
    This reverts commit 8f14ac1bf74c439b300f5f3b5be82bb999389f72.

--- src/storage/Filter/WorldDataFilter.ts
+++ src/storage/Filter/WorldDataFilter.ts
@@ -1,14 +1,14 @@
 import WorldData from '../WorldData';
-// import CircleInterface from '../../physics/CircleInterface';
-// import PlayerData from '../PlayerData';
+import CircleInterface from '../../physics/CircleInterface';
+import PlayerData from '../PlayerData';
 import { truncateFloat } from '../../utils';
-// import AsteroidData from '../AsteroidData';
+import AsteroidData from '../AsteroidData';
 
 class WorldDataFilter {
   public filter(worldData: WorldData): object {
     const data = {
       playersData: {},
-      // asteroidsData: {},
+      asteroidsData: {},
       serverStatistics: worldData.serverStatistics,
     };
 
@@ -21,13 +21,12 @@ class WorldDataFilter {
       };
     }
 
-    // TODO
-    // for (const [key, asteroidData] of Object.entries(worldData.asteroidsData)) {
-    //   data.asteroidsData[key] = {
-    //     x: truncateFloat(asteroidData.x),
-    //     y: truncateFloat(asteroidData.y),
-    //   };
-    // }
+    for (const [key, asteroidData] of Object.entries(worldData.asteroidsData)) {
+      data.asteroidsData[key] = {
+        x: truncateFloat(asteroidData.x),
+        y: truncateFloat(asteroidData.y),
+      };
+    }
 
     return data;
   }
