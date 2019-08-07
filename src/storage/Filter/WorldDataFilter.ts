8f14ac1bf74c439b300f5f3b5be82bb999389f72
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-08-07 23:59:34 +0300

    tmp disable asteroids sync

--- src/storage/Filter/WorldDataFilter.ts
+++ src/storage/Filter/WorldDataFilter.ts
@@ -1,14 +1,14 @@
 import WorldData from '../WorldData';
-import CircleInterface from '../../physics/CircleInterface';
-import PlayerData from '../PlayerData';
+// import CircleInterface from '../../physics/CircleInterface';
+// import PlayerData from '../PlayerData';
 import { truncateFloat } from '../../utils';
-import AsteroidData from '../AsteroidData';
+// import AsteroidData from '../AsteroidData';
 
 class WorldDataFilter {
   public filter(worldData: WorldData): object {
     const data = {
       playersData: {},
-      asteroidsData: {},
+      // asteroidsData: {},
       serverStatistics: worldData.serverStatistics,
     };
 
@@ -21,12 +21,13 @@ class WorldDataFilter {
       };
     }
 
-    for (const [key, asteroidData] of Object.entries(worldData.asteroidsData)) {
-      data.asteroidsData[key] = {
-        x: truncateFloat(asteroidData.x),
-        y: truncateFloat(asteroidData.y),
-      };
-    }
+    // TODO
+    // for (const [key, asteroidData] of Object.entries(worldData.asteroidsData)) {
+    //   data.asteroidsData[key] = {
+    //     x: truncateFloat(asteroidData.x),
+    //     y: truncateFloat(asteroidData.y),
+    //   };
+    // }
 
     return data;
   }
