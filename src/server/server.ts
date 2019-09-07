746f4e7b12c38ffe979e6515a27ea3f2b51b8467
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-09-08 00:26:39 +0300

    cs fixes

--- src/server/server.ts
+++ src/server/server.ts
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
   if (storage.players.size >= config.maxPlayers) {
     socket.emit('notification', {
       type: 'error',
-      message: 'Server is overloaded. Please try again later.'
+      message: 'Server is overloaded. Please try again later.',
     });
     logger.warn('Server is overloaded. Player was rejected to connect');
     socket.disconnect();
